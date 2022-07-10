09dccb0bc71e566d5236d9ee56026a5a61fe9a6c
anthony4tner <adfortner@protonmail.com>
2022-07-10 11:06:12 -0700

    finished hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,45 @@
-import Layout from '../components/layout'
+// import Layout from '../components/layout'
 import { attributes, html } from '../content/home.md'
 
-const Home = () => (
+// const Home = () => (
+//   <Layout>
+//     <h1>{attributes.title}</h1>
+//     <div dangerouslySetInnerHTML={{ __html: html }} />
+//     <style jsx>{`
+//       h1,
+//       div {
+//         text-align: center;
+//       }
+//     `}</style>
+//   </Layout>
+// )
+
+// export default Home
+
+import * as React from "react"
+import { Parallax } from "@react-spring/parallax"
+import Layout from "../src/components/layout"
+import Hero from "../src/components/hero"
+// import Projects from "../components/projects"
+// import About from "../components/about"
+// import Contact from "../components/contact"
+
+const Illiac = () => (
   <Layout>
-    <h1>{attributes.title}</h1>
-    <div dangerouslySetInnerHTML={{ __html: html }} />
-    <style jsx>{`
-      h1,
-      div {
-        text-align: center;
-      }
-    `}</style>
+    <Parallax pages={5}>
+      {/* <div dangerouslySetInnerHTML={{ __html: html }} />
+      <style jsx>{`
+        h1,
+        div {
+          text-align: center;
+        }
+      `}</style> */}
+      <Hero offset={0} factor={1} />
+      {/* <Projects offset={1} factor={2} />
+      <About offset={3} factor={1} />
+      <Contact offset={4} factor={1} /> */}
+    </Parallax>
   </Layout>
 )
 
-export default Home
+export default Illiac
