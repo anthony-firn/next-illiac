2821ad7dec4d8c688e94e9624cb7b84d094c27bd
anthony4tner <adfortner@protonmail.com>
2022-07-10 12:04:22 -0700

    added project cards

--- content/home.md
+++ content/home.md
@@ -3,6 +3,6 @@ title: Home
 date: 2019-03-17T19:31:20.591Z
 ---
 
-## Welcome to the Home Page
+# Hi, I'm Anthony Fortner
 
-If you want to know more about Next.js + Netlify CMS go to their [official tutorial](https://www.netlifycms.org/docs/nextjs/).
+and this is a portfolio of various projects and interests of mine
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ import * as React from "react"
 import { Parallax } from "@react-spring/parallax"
 import Layout from "../src/components/layout"
 import Hero from "../src/components/hero"
-// import Projects from "../components/projects"
+import Projects from "../src/components/projects"
 // import About from "../components/about"
 // import Contact from "../components/contact"
 
@@ -35,8 +35,8 @@ const Illiac = () => (
         }
       `}</style> */}
       <Hero offset={0} factor={1} />
-      {/* <Projects offset={1} factor={2} />
-      <About offset={3} factor={1} />
+      <Projects offset={1} factor={2} />
+      {/* <About offset={3} factor={1} />
       <Contact offset={4} factor={1} /> */}
     </Parallax>
   </Layout>
