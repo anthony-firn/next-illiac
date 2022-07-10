b054b4a6691edbbfba3dda58f92c537b2cef6ce2
anthony4tner <adfortner@protonmail.com>
2022-07-10 16:23:52 -0700

    added about section

--- content/about.md
+++ content/about.md
@@ -3,6 +3,6 @@ title: About
 date: 2019-03-17T19:31:20.591Z
 ---
 
-## Welcome to the About page
+## Working Together 👋
 
-This is the about page.
+> Please reach out if you have any questions for me. Hope to hear from you soon!
--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ import { Parallax } from "@react-spring/parallax"
 import Layout from "../src/components/layout"
 import Hero from "../src/components/hero"
 import Projects from "../src/components/projects"
-// import About from "../components/about"
+import About from "../src/components/about"
 // import Contact from "../components/contact"
 
 const Illiac = () => (
@@ -36,8 +36,8 @@ const Illiac = () => (
       `}</style> */}
       <Hero offset={0} factor={1} />
       <Projects offset={1} factor={2} />
-      {/* <About offset={3} factor={1} />
-      <Contact offset={4} factor={1} /> */}
+      <About offset={3} factor={1} />
+      {/* <Contact offset={4} factor={1} /> */}
     </Parallax>
   </Layout>
 )
