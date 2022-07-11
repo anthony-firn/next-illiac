7c070f7595e5232a62d27d8345efc73ac4e48364
anthony4tner <adfortner@protonmail.com>
2022-07-11 13:24:23 -0700

    added theme ui

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
+const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
 module.exports = {
   webpack: (configuration) => {
     configuration.module.rules.push({
       test: /\.md$/,
       use: 'frontmatter-markdown-loader',
+    })
+    return configuration
+  },
+  withMDX: (configuration) => {
+    configuration.module.rules.push({
+      pageExtensions: ['js', 'mdx']
     })
     return configuration
   },
